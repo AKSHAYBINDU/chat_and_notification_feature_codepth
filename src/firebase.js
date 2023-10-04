12c4defeee062dc31710212e8a2b2031a9af216e
AKSHAYBINDU <akshaykumarkanad@gmail.com>
2023-10-04 14:56:43 +0530

    changes in fcm.

--- src/firebase.js
+++ src/firebase.js
@@ -51,7 +51,8 @@ requestPermission();
 
   if ("serviceWorker" in navigator) {
     navigator.serviceWorker
-      .register("/firebase-messaging-sw.js")
+      .register("/firebase-messaging-sw.js",{
+        scope: '/'})
       .then((registration) => {
         console.log("Service Worker registered with scope:", registration.scope);
       })
